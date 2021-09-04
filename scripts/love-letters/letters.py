972632cd0bf79251911e914fe39aba4c6ab89fa8
Trevor Flowers <trevor@trevor.smith.name>
2021-09-04 14:52:45 -0700

    Added an example script: love letters

---
+++ scripts/love-letters/letters.py
@@ -0,0 +1,34 @@
+LETTERS = [
+    [
+        "",
+        "I only have eyes",
+        "for you."
+    ],
+    [
+        "",
+        "You are seen",
+        "and loved."
+    ],
+    [
+        "The day we met",
+        "was pretty lucky",
+        "because we found",
+        "each other."
+    ],
+    [
+        "If the stars fell",
+        "I'd use a big",
+        "flashlight to find",
+        "you.",
+    ],
+    [
+        "",
+        "Your morning breath",
+        "is no big deal.",
+    ],
+    [
+        "",
+        "Thank dog for",
+        "the house cleaner.",
+    ],
+]
